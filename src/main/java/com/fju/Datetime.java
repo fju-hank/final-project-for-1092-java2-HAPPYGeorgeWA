c70ba34160156fbcc1d43fa08e3f996274dbff37
HAPPYGeorgeWA <godspeed246@gmail.com>
2021-06-28 11:57:38 +0800

    add Datetime

--- src/main/java/com/fju/Datetime.java
+++ src/main/java/com/fju/Datetime.java
@@ -1,4 +1,14 @@
 package com.fju;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Datetime {
+
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+    Date current = new Date();
+
+    public void printdate() {
+        System.out.println(sdf.format(current));
+    }
 }
