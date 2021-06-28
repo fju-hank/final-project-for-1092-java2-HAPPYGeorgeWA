66e1ecb48411478b4c6cacc1cb246116a2a1cbeb
HAPPYGeorgeWA <godspeed246@gmail.com>
2021-06-28 11:42:14 +0800

    add User array

--- src/main/java/com/fju/ATM.java
+++ src/main/java/com/fju/ATM.java
@@ -1,7 +1,21 @@
 package com.fju;
 
 public class ATM {
-    User user1 = new User("George","11111",12345,200000);
-    User user2 = new User("Zofia","22222",54321,200000);
+    private User[]users = new User[100];
 
+    private int size = 0;
+
+    private User atmuser;
+
+    public ATM() {
+        starting();
+    }
+    public void starting(){
+        User user1 = new User("George","11111",12345,200000);
+        User user2 = new User("Zofia","22222",54321,200000);
+        users[0]=user1;
+        users[1]=user2;
+        size=2;
+
+    }
 }
